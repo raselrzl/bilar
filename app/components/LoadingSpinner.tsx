6228ee8b6015d93c29f444e0b19305a6c23d8ee7
Rasel Miah <rasel6041@gmail.com>
2025-08-15 16:04:40 +0100

    updated database

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,4 +1,4 @@
-export default function LoadingSpinner() {
+/* export default function LoadingSpinner() {
   return (
     <div className="flex justify-center items-center py-10">
       <svg
@@ -8,7 +8,14 @@ export default function LoadingSpinner() {
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
         <path
           className="opacity-75"
           fill="currentColor"
@@ -18,3 +25,4 @@ export default function LoadingSpinner() {
     </div>
   );
 }
+ */
