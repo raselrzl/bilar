e72725d26c02c595cf8f5f1f945b7fe448f41574
Rasel Miah <rasel6041@gmail.com>
2025-08-15 15:44:51 +0100

    updated database

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Foooter from "./components/Footer";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Inter } from 'next/font/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
