b1686f00c3b6236c086dc050e3f3f4b25ffc673b
Rasel Miah <rasel6041@gmail.com>
2025-08-17 09:25:30 +0100

    contact message error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}', // Adjust paths based on your project structure
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    // Comment out or remove any plugins temporarily to isolate the issue
-    // require('@tailwindcss/oxide'), // Remove this line
-  ],
-}
+// tailwind.config.ts
+export default {
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
