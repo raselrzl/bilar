99e2bc1eb52be999d52b30b964b28df9c21d7486
Rasel Miah <rasel6041@gmail.com>
2025-08-25 15:09:48 +0100

    contact message error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,6 +6,13 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NavLink } from "./NavLink";
 import { UserDropdown } from "./UserDropdown";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { User } from "lucide-react";
 
 export default async function Navbar() {
   const { getUser } = getKindeServerSession();
@@ -62,13 +69,21 @@ export default async function Navbar() {
             </>
           ) : (
             <div className="flex md:flex md:flex-1 md:items-center md:justify-end md:space-x-1">
-              <Button variant="ghost" asChild>
-                <LoginLink>Sign in</LoginLink>
-              </Button>
-              <span className="h-6 w-px bg-gray-400 self-center"></span>
-              <Button variant="ghost" asChild>
-                <RegisterLink>Register</RegisterLink>
-              </Button>
+              <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="text-lg">
+                      <User className="w-6 h-6 text-gray-700" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem asChild>
+                      <LoginLink>Sign in</LoginLink>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                      <RegisterLink>Register</RegisterLink>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
             </div>
           )}
         </div>
