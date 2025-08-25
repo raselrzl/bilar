f26913b68c379875fe58fa1c7e8969249f8da766
Rasel Miah <rasel6041@gmail.com>
2025-08-25 09:34:36 +0100

    contact message error

--- app/components/UserDropDownMenu.tsx
+++ app/components/UserDropDownMenu.tsx
@@ -2,11 +2,21 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { CircleUser } from "lucide-react";
-import { LoginLink, LogoutLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
-
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 interface User {
   given_name: string;
@@ -23,13 +33,19 @@ export default function UserDropDownMenu({ user }: { user: User | null }) {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full mr-5 bg-red-800">
+          <Button
+            variant="secondary"
+            size="icon"
+            className="rounded-full mr-5 bg-red-800"
+          >
             <CircleUser className="w-5 h-5 text-white" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>{userData.given_name}</DropdownMenuLabel>
-          <DropdownMenuLabel className="text-xs text-gray-600">{userData.email}</DropdownMenuLabel>
+          <DropdownMenuLabel className="text-xs text-gray-600">
+            {userData.email}
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem asChild>
             <LogoutLink>Sign out</LogoutLink>
@@ -41,16 +57,24 @@ export default function UserDropDownMenu({ user }: { user: User | null }) {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full mr-5 bg-red-800">
+          <Button
+            variant="secondary"
+            size="icon"
+            className="rounded-full mr-5 bg-red-800"
+          >
             <CircleUser className="w-5 h-5 text-white" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuItem asChild>
-            <LoginLink>Sign in</LoginLink>
+            <Button variant="ghost" asChild>
+              <LoginLink>Sign in</LoginLink>
+            </Button>
           </DropdownMenuItem>
           <DropdownMenuItem asChild>
-            <RegisterLink>Reg</RegisterLink>
+            <Button variant="ghost" asChild>
+              <RegisterLink>Reg</RegisterLink>
+            </Button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
