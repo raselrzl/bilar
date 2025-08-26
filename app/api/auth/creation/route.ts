cddb85f99cd75e1bd4fe3c10cb59d5f4c6346c2c
Rasel Miah <rasel6041@gmail.com>
2025-08-26 10:48:54 +0100

    contact message error

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://www.bilvio.se/"
+      : "https://www.bilvio.se/api/auth/login"
   );
 }
