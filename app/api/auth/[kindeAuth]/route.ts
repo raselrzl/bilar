a42c864812cc992077f4fdb10869b969af15d928
Rasel Miah <rasel6041@gmail.com>
2025-08-28 08:09:41 +0100

    contact message error

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,2 @@
-import { NextRequest } from "next/server";
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(request: NextRequest, { params }: { params: { kindeAuth: string } }) {
-  const endpoint = params.kindeAuth;
-  return await handleAuth(request, endpoint);
-}
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
